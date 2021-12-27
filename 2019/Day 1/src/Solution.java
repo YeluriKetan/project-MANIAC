916ee41be2cc1d26f293e16a0f0faa221d0a1147
YeluriKetan <e0603110@u.nus.edu>
2021-12-27 18:58:02 +0800

    Add 2019 Day 1

---
+++ 2019/Day 1/src/Solution.java
@@ -0,0 +1,93 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.Paths;
+import java.util.StringTokenizer;
+
+public class Solution {
+
+    private static FastScan fs;
+
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        File file = new File(Paths.get("src", "input.txt").toString());
+
+        fs = new FastScan(file);
+        System.out.println(sol.part1());
+
+        fs.changeFile(file);
+        System.out.println(sol.part2());
+        fs.close();
+    }
+
+    private int part1() {
+        int total = 0;
+        for (int i = 0; i < 100; i++) {
+            total += (fs.nextInt() / 3) - 2;
+        }
+        return total;
+    }
+
+    private int part2() {
+        int total = 0;
+        for (int i = 0; i < 100; i++) {
+            int curr = fs.nextInt();
+            while (true) {
+                curr = (curr / 3) - 2;
+                if (curr > 0) {
+                    total += curr;
+                } else {
+                    break;
+                }
+            }
+        }
+        return total;
+    }
+
+    private static class FastScan {
+        private BufferedReader br;
+        private StringTokenizer st;
+
+        private FastScan(File file) {
+            try {
+                br = new BufferedReader(new FileReader(file));
+            } catch (FileNotFoundException ex) {
+                ex.printStackTrace();
+            }
+        }
+
+        private void changeFile(File file) {
+            close();
+            try {
+                br = new BufferedReader(new FileReader(file));
+            } catch (FileNotFoundException ex) {
+                ex.printStackTrace();
+            }
+        }
+
+        private String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException ex) {
+                    ex.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        private int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        private void close() {
+            try {
+                br.close();
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+        }
+    }
+}
